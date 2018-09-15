7403ec8198bfe9b9c1f4eacba7c5f0eaf4ab3581
David Baas <David@Rogers-MacBook-Pro.local>
2018-09-14 23:59:21 -0400

    unfinished readfile

--- Ceaser-Cipher-Prj/decipher.c
+++ Ceaser-Cipher-Prj/decipher.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "decipher.h"
 
 void readFreq(float found[], FILE* letFreq){
-    printf("here1");
     char throwaway_buffer[3];
+
+    //Throws away the first two chars of the file. They are not used.
     fgetc(letFreq);
     fgetc(letFreq);
+
     for(int index = 0; index < NUM_LETTERS; ++index){
         printf("here2");
         fscanf(letFreq,"%f", &found[index]);
         for(int j = 0; j < 2; ++j){
             fgetc(letFreq);
         }
-        printf("here3");
     }
 }
 
 void calcFreq(float found[], FILE* datafile){
+    char observed;
 
-}
+    while(observed = fgetc(datafile), observed != EOF){
+        if(isupper(observed)){
+            
+        }
+    }    
 
-// char rotate(char ch, int num){
+    // char rotate(char ch, int num){
 
-// }
+    // }
 
-// int findKey(float given[], float found[]){
+    // int findKey(float given[], float found[]){
 
-// }
+    // }
 
-void decrypt(int key, FILE* datafile, FILE* outfile){
+    void decrypt(int key, FILE* datafile, FILE* outfile){
 
-}
+    }
