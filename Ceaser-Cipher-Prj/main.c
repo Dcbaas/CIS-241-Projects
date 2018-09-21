925cfa3f254bf07026a98061b52f2945cb09ec06
David Baas <David@Rogers-MacBook-Pro.local>
2018-09-20 22:41:18 -0400

    Added more detailed comments to main

--- Ceaser-Cipher-Prj/main.c
+++ Ceaser-Cipher-Prj/main.c
@@ -20,15 +20,19 @@
  * arguments is not exactly 3
  *********************************************************************/
 int main(int argc, char** argv){
+    //Validate the number of arguments.
     if(argc != 3){
         fprintf(stderr, "INVALID NUMBER OF PARAMETERS\n");
         fprintf(stderr, "USAGE: ./a.out input_file output_file\n");
         return EPERM;
     }
 
+    //Define all the files and open two of them.
     FILE* frequency_values = fopen("letFreq.txt", "r");
     FILE* cipher_text = fopen(argv[1], "r");
     FILE* output_file;
+
+    //Check that both frequency and cipher text opened correctly.
     if(frequency_values == NULL){
         fprintf(stderr, "ERROR LOADING FREQUENCY VALUES\n\n");
         return EIO;
@@ -38,23 +42,36 @@ int main(int argc, char** argv){
         return EIO;
     }
 
+    //Define two arrays for given and observed letter frequencies.
     float default_freq[NUM_LETTERS];
     float observed_freq[NUM_LETTERS];
+
+    //Read in the given frequencies and close the frequency file.
     readFreq(default_freq, frequency_values);
     fclose(frequency_values);
 
+    //Calculate the frequency for the cipher text and find the best
+    //offset.
     calcFreq(observed_freq, cipher_text);
     int best_key = findKey(default_freq, observed_freq);
 
+    //Open the output file now and validate it was opened correctly.
+    //I do this later from the others because my perceiption is that
+    //the less time I have a file open the better. File I/O is 
+    //very tempermental I think.
     output_file = fopen(argv[2], "w");
     if(!output_file){
         fprintf(stderr, "ERROR CREATING OUTPUT FILE");
         return EIO;
     }
 
+    //Decrypt the cipher text and write it to the output file.
     decrypt(best_key, cipher_text, output_file);
 
+    //Close all of the remaining open files.
     fclose(cipher_text);
     fclose(output_file);
+
+    //Success.
     return 0;
 }
