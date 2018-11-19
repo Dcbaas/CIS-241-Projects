230d6ff69208d52a49ab06afaabc5eb0687da8e4
Dcbaas <Dcbaas@github.com>
2018-11-19 15:39:36 -0500

    finished lab 9

---
+++ Labs/lab9/final/bash_script.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+
+sort -t, -k2 lab9.dat >lab9a.dat
+gawk -F, '{printf "%s\t%s\t%s\n", $3, $2, $1}' lab9a.dat >lab9b.dat
+sed -n -e 's/Fall /F/p' -e 's/Winter /W/p' lab9b.dat >lab9c.dat
+
+test_path=course/cis241/example/blah/foo/demo.c
+echo $test_path | gawk -F/ '{print $NF}'
