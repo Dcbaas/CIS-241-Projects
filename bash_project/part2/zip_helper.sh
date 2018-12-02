8fa4cd9822fb381bbe91853dde1734b0debf2465
Dcbaas <Dcbaas@github.com>
2018-12-02 13:05:03 -0500

    solved it but need to implement

--- bash_project/part2/zip_helper.sh
+++ bash_project/part2/zip_helper.sh
@@ -1,14 +1,33 @@
 #/usr/bin/bash
 
 function create_student_dirs(){
+  #I need two types of field sperators for this function. One is the 
+  #normal whitespace sperator and the other is a newline only 
+  #Seperator. 
   OLD_IFS=IFS
   IFS=$'\n'
   for F in $(ls temp/ -1)
   do
-    echo $(echo $F | gawk -F_ '{print $NF}') #change this to just store the name and the last part of the line to another var
-    #Check the username to create a folder
+    argc=$(echo $F | gawk -F_ '{print NF}') 
+    student=$(echo $F | gawk -F_ '{print $2}')
+    filename=$(echo $F | gawk -F_ '{print $NF}')
+
+    #Check if the directory exist. Create it if it doesn't 
+    if [ -d grading/$student ]
+    then
+      echo exist
+    else
+      mkdir grading/$student
+      echo "created $student"
+    fi
+
+    #The memo file will always have one less seperator (4) than the 
+    #rest of them so that is the check for the file
+    #transfer the files
+
     #check the last bit for the regular expression
     #Write the file to the folder, rename to memo if needed
+    IFS=$'\n'
   done
 }
 
